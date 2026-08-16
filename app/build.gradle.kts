24ddc7e744d66cf26833bfe3d67bb458d4644cfb
rjkyker383-gif <rjkyker383@gmail.com>
2026-08-16 18:51:36 -0400

    Add buildable Android app module

---
+++ app/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.rjkyker.shizukuai.broker"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.rjkyker.shizukuai.broker"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("dev.rikka.shizuku:api:13.1.5")
+    implementation("dev.rikka.shizuku:provider:13.1.5")
+    testImplementation("junit:junit:4.13.2")
+}
