667433b3a8e61bcfd1dd77ee0b77430ad2e1ce44
rjkyker383-gif <rjkyker383@gmail.com>
2026-08-17 05:50:56 -0400

    Make broker risk policy authoritative

--- app/src/main/java/com/rjkyker/shizukuai/broker/ActionAllowList.kt
+++ app/src/main/java/com/rjkyker/shizukuai/broker/ActionAllowList.kt
@@ -2,17 +2,21 @@ package com.rjkyker.shizukuai.broker
 
 object ActionAllowList {
 
-    private val allowedActions = setOf(
-        "GET_DEVICE_INFO",
-        "GET_PACKAGE_LIST",
-        "GET_CURRENT_APP",
-        "OPEN_APP",
-        "OPEN_SETTINGS",
-        "GET_BATTERY_INFO",
-        "GET_DISPLAY_INFO"
+    private val actionRiskLevels = mapOf(
+        "GET_DEVICE_INFO" to RiskLevel.LOW,
+        "GET_PACKAGE_LIST" to RiskLevel.LOW,
+        "GET_CURRENT_APP" to RiskLevel.LOW,
+        "OPEN_APP" to RiskLevel.MEDIUM,
+        "OPEN_SETTINGS" to RiskLevel.MEDIUM,
+        "GET_BATTERY_INFO" to RiskLevel.LOW,
+        "GET_DISPLAY_INFO" to RiskLevel.LOW
     )
 
     fun isAllowed(action: String): Boolean {
-        return action in allowedActions
+        return action in actionRiskLevels
+    }
+
+    fun riskLevelFor(action: String): RiskLevel? {
+        return actionRiskLevels[action]
     }
 }
